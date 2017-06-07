b8eeb3d8d6ab59a2cd290350414fb2b2c4b13c8c
Herbert Caffarel <herbert.caffarel@cicef.pro>
2017-06-07 09:20:49 +0200

    Ajout classe HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Classe de test pour afficher le sempiternel 
+ * Hello World!
+ */
+public class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("Hello Wolrd!");
+	}
+}
